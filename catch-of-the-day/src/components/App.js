7ffb5e7f282e4f5bbca3291dba8c90a9c18a5a5d
Phos23 <phos23@gmail.com>
2021-10-25 20:33:35 +0100

    add local storage to read order

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,17 +11,30 @@ class App extends React.Component {
     fishes: {},
     order: {}
   };
+
   componentDidMount() {
     const { params } = this.props.match;
+    // first reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: "fishes"
     });
   }
 
+  componentDidUpdate() {
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
+  }
+
   componentWillUnmount() {
     base.removeBinding(this.ref);
   }
+
 
   addFish = fish => {
     // 1. Take a copy of the existing state
