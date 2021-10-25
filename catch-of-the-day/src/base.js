a50e82798a03e7c97cb3faed0e0b11e3eadb3ffe
Phos23 <phos23@gmail.com>
2021-10-25 19:22:31 +0100

    update and setup firebase

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,46 +1,30 @@
-// import Rebase from 're-base';
-import firebase from 'firebase';
+import Rebase from "re-base";
+import firebase from "firebase";
 
-// const firebaseApp = firebase.initializeApp({
-//     apiKey: "AIzaSyAO6AaWDobKzY_TgvlUFE2VK8ErU_bmc8w",
-//     authDomain: "catch-of-the-day-phos23.firebaseapp.com",
-//     databaseURL: "https://catch-of-the-day-phos23-default-rtdb.europe-west1.firebasedatabase.app",
-// })
-
-// const base = Rebase.createClass(firebaseApp.database());
-
-// // This is a named export
-// export { firebaseApp }
-
-// //This is a default export;
-// export default base; 
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyAO6AaWDobKzY_TgvlUFE2VK8ErU_bmc8w",
+
     authDomain: "catch-of-the-day-phos23.firebaseapp.com",
+  
     databaseURL: "https://catch-of-the-day-phos23-default-rtdb.europe-west1.firebasedatabase.app",
+  
     projectId: "catch-of-the-day-phos23",
+  
     storageBucket: "catch-of-the-day-phos23.appspot.com",
+  
     messagingSenderId: "343602860339",
-    appId: "1:343602860339:web:254169880093e7197fee46",
-    measurementId: "G-3VLLEEXQX9"
-};
+  
+    appId: "1:343602860339:web:b2fcfc4cb8e55f067fee46",
+  
+    measurementId: "G-WSJHSN5VF1"
+  
+  
+});
 
 const base = Rebase.createClass(firebaseApp.database());
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // This is a named export
-export { firebaseApp }
+export { firebaseApp };
 
-//This is a default export;
+// this is a default export
 export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Header from './Header';
-import Order from './Order';
-import Inventory from './Inventory';
-import sampleFishes from '../sample-fishes'
-import Fish from './Fish';
-import base from '../base';
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
   state = {
@@ -12,31 +12,38 @@ class App extends React.Component {
     order: {}
   };
   componentDidMount() {
-    this.ref = base.syncState();
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes"
+    });
   }
 
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
   addFish = fish => {
-    // 1. Take a copy of the existing this.state
+    // 1. Take a copy of the existing state
     const fishes = { ...this.state.fishes };
-    // 2. Add out new fish to that fishes variable
+    // 2. Add our new fish to that fishes variable
     fishes[`fish${Date.now()}`] = fish;
     // 3. Set the new fishes object to state
     this.setState({ fishes });
   };
 
   loadSampleFishes = () => {
-    this.setState({ fishes: sampleFishes })
-  }
+    this.setState({ fishes: sampleFishes });
+  };
 
-  addToOrder = (key) => {
-    // 1. Take a copy of state
-    const order = { ...this.state.order }
-    // 2. Either add to the order, update the number in our order
+  addToOrder = key => {
+    // 1. take a copy of state
+    const order = { ...this.state.order };
+    // 2. Either add to the order, or update the number in our order
     order[key] = order[key] + 1 || 1;
     // 3. Call setState to update our state object
-    this.setState({ order })
-  }
+    this.setState({ order });
+  };
 
   render() {
     return (
@@ -49,18 +56,18 @@ class App extends React.Component {
                 key={key}
                 index={key}
                 details={this.state.fishes[key]}
-                addToOrder={this.addToOrder} />))}
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
-        <Order
-          fishes={this.state.fishes}
-          order={this.state.order}
-        />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
-          loadSampleFishes={this.loadSampleFishes} />
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
-    )
+    );
   }
 }
 
